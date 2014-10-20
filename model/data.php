f64986ffd53676933073c6eaf853347f961cd928
Laurent Treguier <laurent.treguier@isen.fr>
2014-10-20 09:24:08 +0200

    YOLO... le retour

--- model/data.php
+++ model/data.php
@@ -1,36 +1,11 @@
 <?php
-<<<<<<< HEAD
     require_once "table.php";
     
     class Data extends Table
     {
-        public function __construct($id)
+        function __construct($id)
         {
             parent::__construct("data", $id);
         }
     }
 ?>
-=======
-
-class data{
-	private $members;
-
-
-	function __construct($id){
-		$mysqli = new mysqli("127.0.0.1", "root", "", "rentree");
-		
-		$this->members = $mysqli->query("SELECT * FROM data WHERE id=".$id)->fetch_assoc();
-		/*foreach ($members as $key => $value) {
-			echo $key;
-		}*/
-
-	}
-
-	public function __get($member)
-	{
-		return $this->members[$member];
-	}
-
-}
-?>
->>>>>>> bce8ec8592fdb5b4aeec8abd8001aa57a1f9eea1
--- model/document.php
+++ model/document.php
@@ -1,32 +1,11 @@
 <?php
-<<<<<<< HEAD
     require_once "table.php";
     
-    class Document extends Table
+    class Data extends Table
     {
-        public function __construct($id)
+        function __construct($id)
         {
             parent::__construct("document", $id);
         }
     }
 ?>
-=======
-
-class document{
-	private $doc;
-
-
-	function __construct($id){
-		$mysqli = new mysqli("127.0.0.1", "root", "", "rentree");
-		
-		$this->doc = $mysqli->query("SELECT * FROM document WHERE id=".$id)->fetch_assoc();
-	}
-
-	public function __get($arg)
-	{
-		return $this->doc[$arg];
-	}
-
-
-}
->>>>>>> bce8ec8592fdb5b4aeec8abd8001aa57a1f9eea1
